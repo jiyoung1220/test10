669c9a0b2be1c06588d235a691ca4e34a3488805
jy <jjyung1220@gmail.com>
2022-09-27 00:21:09 +0900

    [hotfix] tag에서 오류 수정

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,5 +7,6 @@ public class main {
         //feature : 기능개발하고 수정함
         //release : 전체 pull 받고 현재 dev브랜치로 변경 후 release 생성
         // feature : release에서 버그발생하여 추가 수정
+        //hotfix : tag 찍었는데 추가 수정이 있을 경우
     }
 }
